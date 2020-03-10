8a02812f0c898d15b952dfc1c3dfbe470d1e0e5a
Steve Min <stevekmin@gmail.com>
2020-03-10 16:10:28 -0700

    setup authentication

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PHP - Instagram
                 </div>
 
                 <div class="links">
