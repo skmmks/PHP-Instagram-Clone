ac23d53364a818eb14018c2a88a2deeeb48f189c
Steve Min <stevekmin@gmail.com>
2020-03-11 11:02:59 -0700

    import package for login styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-3">
+            <!-- <img src="https://havecamerawilltravel.com/photographer/files/2016/04/Instagram-Logo-2016.jpg" alt=""> -->
+        </div>
+        <div class="col-9">
+        
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                   <div style="border-right: 1px solid" class="pr-3">Logo PlaceHolder</div>
+                   <div class="pl-3">PHP - Instagram</div> 
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
