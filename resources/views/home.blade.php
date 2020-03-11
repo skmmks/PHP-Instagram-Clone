93bad67ed42751e7cc072146f23bbcc6a7ca1b31
Steve Min <stevekmin@gmail.com>
2020-03-11 15:05:08 -0700

    add styling for front end login user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,13 @@
         <div class="col-3">
             <img class="instagram-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/58/Instagram-Icon.png" alt="">
         </div>
-        <div class="col-9">
-        
+        <div class="col-9 pt-5">
+            <div><h1>PHP-Instagram</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"> <strong>200</strong> posts</div>
+                <div class="pr-5"><strong>50k</strong> followers  </div>
+                <div class="pr-5"> <strong>200</strong> following</div>
+            </div>
         </div>
     </div>    
 </div>
